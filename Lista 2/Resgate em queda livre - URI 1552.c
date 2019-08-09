1278e42b01b336751172953389e3bfa4f4c3d724
dayvid-lopes <dayvidlopes@softeam.com.br>
2019-08-09 17:22:57 -0300

    Ajuste do arquivo de resultados

---
+++ Lista 2/Resgate em queda livre - URI 1552.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    FILE *resultado;
+    resultado = fopen("Lista 2/resultados/resultado 1552", "w");
+    if(resultado == NULL){
+        return 1;
+    }
+    int 
+    fclose(resultado);
+    return 0;
+}
