0808cd70498d6c345a335115b66fe1d66d9bf8af
dayvid-lopes <dayvidlopes@softeam.com.br>
2019-08-11 00:56:42 -0300

    Função para remover raiz

--- Lista 2/Resgate em queda livre - URI 1552.c
+++ Lista 2/Resgate em queda livre - URI 1552.c
@@ -11,6 +11,8 @@ typedef struct typevertices
 {
     int status;
     int numero;
+    int x;
+    int y;
 }Pessoas;
 
 typedef struct typearesta{
@@ -25,14 +27,23 @@ typedef struct typeheap Heap;
 struct typeheap{
     Aresta *aresta;
     int Nfilhos;
+    Heap *pai;
     Heap *direita;
     Heap *esquerda;
+};
+
+typedef struct typepilha Pilha;
+
+struct typepilha{
+    Heap *no;
+    Pilha *prox;
 };
 
 #define MALLOC(t,n) (t*) malloc(sizeof(t)*n)
 #define X coordenadas->x
 #define Y coordenadas->y
 #define ARESTA raiz->aresta
+#define PESO aresta->peso
 #define DIREITA raiz->direita
 #define ESQUERDA raiz->esquerda
 #define NFILHOS raiz->Nfilhos
@@ -41,28 +52,35 @@ struct typeheap{
 #define imprima2(texto, valor1, valor2) fprintf(resultado, texto, valor1, valor2)
 #define filhoCompleto(valor) log2(1 - (valor / 2.0) * (1 - 2)) == (int)log2(1 - (valor / 2.0) * (1 - 2))? 1 : 0
 #define ARESTAS(i, j) arestas[n * i + j]
+#define REMOVE(raiz, aresta) raiz = removerRaiz(raiz, aresta)
 
 FILE *resultado;
 
-double distancia(Ponto *ponto1, Ponto *ponto2){
+double distancia(Pessoas *ponto1, Pessoas *ponto2){
     return sqrt(pow(ponto1->x - ponto2->x, 2) + pow(ponto1->y - ponto2->y, 2));
 }
 
-void insert(Heap *raiz, Aresta *nova_aresta){
-    
+Heap* insert(Heap *raiz, Aresta *nova_aresta){
+    if(raiz == NULL){
+        raiz = MALLOC(Heap, 1);
+        ESQUERDA = DIREITA = NULL;
+        ARESTA = nova_aresta;
+        return raiz;
+    }
+
     if(ARESTA->peso > nova_aresta->peso){
         Aresta *aux;
         aux = nova_aresta;
         nova_aresta = ARESTA;
         ARESTA = aux;
     }
+    Heap *filho;
 
     if(NFILHOS < 2){
-        Heap *filho;
         filho = MALLOC(Heap, 1);
         filho->aresta = nova_aresta;
-        filho->direita = NULL;
-        filho->esquerda = NULL;
+        filho->direita = filho->esquerda = NULL;
+        filho->pai = raiz;
         filho->Nfilhos = 0;
         if(NFILHOS == 0){
             ESQUERDA = filho;
@@ -73,21 +91,26 @@ void insert(Heap *raiz, Aresta *nova_aresta){
     }
     else{
         if(ESQUERDA->Nfilhos == DIREITA->Nfilhos){
-            insert(ESQUERDA, nova_aresta);
+            filho = insert(ESQUERDA, nova_aresta);
         }
         else{
             if(filhoCompleto(ESQUERDA->Nfilhos)){
-                insert(DIREITA, nova_aresta);
+                filho = insert(DIREITA, nova_aresta);
             }
             else{
-                insert(ESQUERDA, nova_aresta);
+                filho = insert(ESQUERDA, nova_aresta);
             }
         }
     }
     NFILHOS++;
+    return filho;
 }
 
 void imprimirArvore(Heap *raiz){
+    if(raiz == NULL){
+        imprima("Arvore vazia");
+        return;
+    }
     imprima1("%.2lf\t", ARESTA->peso);
     if(ESQUERDA != NULL){
         imprimirArvore(ESQUERDA);
@@ -98,6 +121,69 @@ void imprimirArvore(Heap *raiz){
     else{
         imprima("\n");
     }
+}
+
+void decrementarPai(Heap *raiz, Heap *pai){
+    if(raiz != pai){
+        decrementarPai(raiz, pai->pai);
+    }
+    pai->Nfilhos--;
+}
+
+void peneirarValor(Heap *raiz){
+    if(NFILHOS < 2){
+        if(NFILHOS == 1){
+            if(ARESTA->peso > ESQUERDA->PESO){
+                Aresta *aux;
+                aux = ARESTA;
+                ARESTA = ESQUERDA->aresta;
+                ESQUERDA->aresta = aux;
+            }
+        }
+    }
+    else{
+        Aresta *aux;
+        if(ESQUERDA->PESO < DIREITA->PESO){
+            aux = ARESTA;
+            ARESTA = ESQUERDA->aresta;
+            ESQUERDA->aresta = aux;
+            peneirarValor(ESQUERDA);
+        }
+        else{
+            aux = ARESTA;
+            ARESTA = DIREITA->aresta;
+            DIREITA->aresta = aux;
+            peneirarValor(DIREITA);
+        }
+    }
+}
+
+Heap* removerRaiz(Heap *raiz, Heap *ultimoElemento){
+    if(NFILHOS == 0){
+        free(ARESTA);
+        free(raiz);
+        return NULL;
+    }
+    
+    free(ARESTA);
+    ARESTA = ultimoElemento->aresta;
+    Heap *pai;
+    pai = ultimoElemento->pai;
+    if(pai->Nfilhos == 2){
+        pai->direita = NULL;
+    }
+    else{
+        pai->esquerda = NULL;
+    }
+    decrementarPai(raiz, pai);
+    free(ultimoElemento);
+
+    peneirarValor(raiz);
+
+    // if(ESQUERDA->aresta->peso < DIREITA->aresta->peso){
+
+    // }
+    return raiz;
 }
 
 int main (){
@@ -116,62 +202,127 @@ int main (){
         scanf("%d", &n);
 
         // Pessoas *pessoas[n];
-        Ponto *coordenadas[n];
+        // Ponto *coordenadas[n];
 
         Aresta *arestas[n][n];
+
+        Pessoas *vertice[n];
 
         int i;
 
         for(i = 0; i < n; i++){
-            coordenadas[i] = MALLOC(Ponto, 1);
-            scanf("%d %d", &coordenadas[i]->x, &coordenadas[i]->y);
+            vertice[i] = MALLOC(Pessoas, 1);
+            scanf("%d %d", &vertice[i]->x, &vertice[i]->y);
         }
         
         double matriz[n*n];
         int j;
         Heap *raiz;
+        raiz = NULL;
+        Pilha *topo;
         for(i = 0; i < n; i++){
+            // vertice1 = MALLOC(Pessoas, 1);
             for(j = 0; j < n; j++){
-                Pessoas *vertice;
+                // Pessoas *vertice2;
                 if(i < j){
                     arestas[i][j] = MALLOC(Aresta, 1);
-                    arestas[i][j]->peso = distancia(coordenadas[i], coordenadas[j]);
+                    arestas[i][j]->peso = distancia(vertice[i], vertice[j]);
                     // imprima1("peso: %lf", arestas[i][j]->peso);
 
-                    vertice = MALLOC(Pessoas, 1);
-                    vertice->numero = i;
-                    arestas[i][j]->vertice1 = vertice;
+                    vertice[i]->numero = i;
+                    arestas[i][j]->vertice1 = vertice[i];
                     
-                    vertice = MALLOC(Pessoas, 1);
-                    vertice->numero = j;
-                    arestas[i][j]->vertice2 = vertice;
+                    // vertice2 = MALLOC(Pessoas, 1);
+                    vertice[j]->numero = j;
+                    arestas[i][j]->vertice2 = vertice[j];
 
-                    if(i == 0 && j == 1){
-                        raiz = MALLOC(Heap, 1);
-                        ARESTA = arestas[i][j];
-                        DIREITA = ESQUERDA = NULL;
-                        NFILHOS = 0;
-                    }
-                    else{
-                        insert(raiz, arestas[i][j]);
-                    }
+                    // if(i == 0 && j == 1){
+                    //     raiz = MALLOC(Heap, 1);
+                    //     ARESTA = arestas[i][j];
+                    //     DIREITA = ESQUERDA = raiz->pai = NULL;
+                    //     NFILHOS = 0;
+                    //     topo = MALLOC(Pilha, 1);
+                    //     topo->no = raiz;
+                    //     topo->prox = NULL;
+                    // }
+                    // else{
+                        // Pilha *aux;
+                        // aux = MALLOC(Pilha, 1);
+                        // aux->no = insert(raiz, arestas[i][j]);
+                        // aux->prox = topo;
+                        // topo = aux;
+                        // if(raiz == NULL){
+                        //     raiz = topo->no;
+                        // }
+                    // }
 
-                    arestas[i][j]->status = 1;
+                    arestas[i][j]->status = 0;
 
                     imprima1("%.2lf\t", arestas[i][j]->peso);
                 }
                 else{
                     arestas[j][i] = arestas[i][j];
+                    if(i == j){
+                        arestas[i][j]->status = 1;
+                    }
                     imprima("\t");
                 }
             }
             imprima("\n");
         }
-        imprima("\n");
-        imprima("\n");
-        imprimirArvore(raiz);
-        imprima("\n");
-        imprima("\n");
+        
+        // while(topo != NULL){
+        //     // imprima1("%.2lf\t", topo->no->aresta->peso);
+        //     Pilha *aux;
+        //     aux = topo;
+        //     topo = aux->prox;
+        //     imprima1("Removendo: %.2lf\n", ARESTA->peso);
+        //     REMOVE(raiz, aux->no);
+        //     free(aux);
+        //     if(topo == NULL){
+        //         raiz = NULL;
+        //     }
+        //     imprima("\n");
+        //     imprima("\n");
+        //     imprimirArvore(raiz);
+        //     imprima("\n");
+        //     imprima("\n");
+        // }
+
+        Pilha *aux;
+        aux = MALLOC(Pilha, 1);
+        aux->no = insert(raiz, arestas[0][1]);
+        aux->prox = topo;
+        topo = aux;
+        if(raiz == NULL){
+            raiz = topo->no;
+        }
+
+        arestas[0][1]->status = -1;
+
+        for(i = 0; i < n; i++){
+            for(j = 0; j < n; j++){
+                if(i < j){
+                    if(arestas[i][j]->status == 0){
+                        aux = MALLOC(Pilha, 1);
+                        aux->no = insert(raiz, arestas[i][j]);
+                        aux->prox = topo;
+                        topo = aux;
+                        if(raiz == NULL){
+                            raiz = topo->no;
+                        }
+                    }
+                }
+            }
+            aux = topo;
+            topo = aux->prox;
+            // imprima1("Removendo: %.2lf\n", ARESTA->peso);
+            REMOVE(raiz, aux->no);
+            free(aux);
+            if(topo == NULL){
+                raiz = NULL;
+            }
+        }
 
         // if(c == 2){
         //     imprima1("teste de casting: %lf\n\n", sqrt(5));
@@ -179,11 +330,11 @@ int main (){
         //     imprima1("teste de casting: %lf\n\n", log2(8));
         // }
         // else {
-            // double valor;
+        //     double valor;
 
-            // int Nfilhos = 6;
-            // valor = filhoCompleto(Nfilhos);
-            // imprima1("teste de casting: %lf\n\n", valor);
+        //     int Nfilhos = 6;
+        //     valor = filhoCompleto(Nfilhos);
+        //     imprima1("teste de casting: %lf\n\n", valor);
         //     if(valor == 0){
         //         imprima("inteiro\n");
         //     }
