0efac2930f954f5c62f9f973259c69f2bf78f5d3
dayvid-lopes <dayvidlopes@softeam.com.br>
2019-08-29 18:31:04 -0300

    Achar e remover menor caminho

--- Lista 2/Quase menor caminho - URI 1391.c
+++ Lista 2/Quase menor caminho - URI 1391.c
@@ -2,16 +2,28 @@
 int n;
 FILE *saida;
 
-int Dijkstra(int matriz[n][n], int distancias[n], int s, int d, int v){
+int comparadorPeso(const void * a, const void * b) {
+    if (*(int*)a == *(int*)b)
+        return 0;
+
+    if (*(int*)a < *(int*)b)
+        return -1;
+
+    return 1;
+}
+
+int Dijkstra(int matriz[n][n], int distancias[n], int q[n], int d, int v){
     int i, peso, menor_peso = -1, vertice;
     for(i = 0; i < n; i++){
         peso = matriz[v][i];
         if(peso > 0){
             if((distancias[i] > distancias[v] + peso) || distancias[i] == -1){
                 distancias[i] = distancias[v] + peso;
-                if(menor_peso > distancias[i]){
+                if(menor_peso == -1 || menor_peso > distancias[i]){
                     menor_peso = distancias[i];
-                    vertice = i;
+                    if(q[i] == 0){
+                        vertice = i;
+                    }
                 }
             }
         }
@@ -20,24 +32,51 @@ int Dijkstra(int matriz[n][n], int distancias[n], int s, int d, int v){
 }
 
 int main(){
-    saida = fopen("resultados/resultado 1391", "w");
+    saida = fopen("Lista 2/resultados/resultado 1391", "w");
     int m;
     scanf("%d %d", &n, &m);
     while(n != 0 && m != 0){
         int s, d, u, v, p, i, j;
         int matriz[n][n], distancias[n], q[n];
         scanf("%d %d", &s, &d);
+        for(i = 0; i < n; i++){
+            for(j = 0; j < n; j++){
+                matriz[i][j] = 0;
+            }
+        }
         for(i = 0; i < m; i++){
             scanf("%d %d %d", &u, &v, &p);
             matriz[u][v] = p;
-            distancias[n] = -1;
+            distancias[i] = -1;
             q[i] = 0;
         }
         distancias[s] = 0;
+        int prox, menor_caminho[n], indice;
+        indice = 0;
+        prox = s;
         for(i = 0; i < n; i++){
-            
-            Dijkstra(matriz, distancias, s, d, s);
+            q[prox] = 1;
+            if(prox != d){
+                menor_caminho[indice++] = prox;
+                prox = Dijkstra(matriz, distancias, q, d, prox);
+            }
         }
+        for(i = 1; i < indice; i++){
+            matriz[i - 1][i] = 0;
+        }
+
+        for(i = 0; i < n; i++){
+            imprima2("i: %d\tdistancia: %d\n", i, distancias[i]);
+        }
+        imprima("\n");
+
+        qsort(distancias, n, sizeof(int), comparadorPeso);
+        
+        for(i = 0; i < n; i++){
+            imprima2("i: %d\tdistancia: %d\n", i, distancias[i]);
+        }
+        imprima("\n");
+
         scanf("%d %d", &n, &m);
     }
     free(saida);
--- Lista 2/cabeçalhos.h
+++ Lista 2/cabeçalhos.h
@@ -3,7 +3,7 @@
 #include <math.h>
 #define MALLOC(t,n) (t*) malloc(sizeof(t)*n)
 #define distancia(ponto1, ponto2) sqrt(pow(ponto1->x - ponto2->x, 2) + pow(ponto1->y - ponto2->y, 2))
-#define imprima(texto) fprintf(resultado, texto)
-#define imprima1(texto, valor1) fprintf(resultado, texto, valor1)
-#define imprima2(texto, valor1, valor2) fprintf(resultado, texto, valor1, valor2)
-#define imprima3(texto, valor1, valor2, valor3) fprintf(resultado, texto, valor1, valor2, valor3)
+#define imprima(texto) fprintf(saida, texto)
+#define imprima1(texto, valor1) fprintf(saida, texto, valor1)
+#define imprima2(texto, valor1, valor2) fprintf(saida, texto, valor1, valor2)
+#define imprima3(texto, valor1, valor2, valor3) fprintf(saida, texto, valor1, valor2, valor3)
