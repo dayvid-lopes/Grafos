86a584c2012b5b809519bd54726b25662f6c801e
dayvid-lopes <dayvidlopes@softeam.com.br>
2019-08-29 11:53:40 -0300

    Começo da implementação do algoritmo de Dijkstra

--- Lista 2/Quase menor caminho - URI 1391.c
+++ Lista 2/Quase menor caminho - URI 1391.c
@@ -2,20 +2,42 @@
 int n;
 FILE *saida;
 
+int Dijkstra(int matriz[n][n], int distancias[n], int s, int d, int v){
+    int i, peso, menor_peso = -1, vertice;
+    for(i = 0; i < n; i++){
+        peso = matriz[v][i];
+        if(peso > 0){
+            if((distancias[i] > distancias[v] + peso) || distancias[i] == -1){
+                distancias[i] = distancias[v] + peso;
+                if(menor_peso > distancias[i]){
+                    menor_peso = distancias[i];
+                    vertice = i;
+                }
+            }
+        }
+    }
+    return vertice;
+}
+
 int main(){
     saida = fopen("resultados/resultado 1391", "w");
     int m;
     scanf("%d %d", &n, &m);
     while(n != 0 && m != 0){
         int s, d, u, v, p, i, j;
-        int matriz[n][n], distancias[n];
+        int matriz[n][n], distancias[n], q[n];
         scanf("%d %d", &s, &d);
         for(i = 0; i < m; i++){
             scanf("%d %d %d", &u, &v, &p);
             matriz[u][v] = p;
             distancias[n] = -1;
+            q[i] = 0;
         }
-        
+        distancias[s] = 0;
+        for(i = 0; i < n; i++){
+            
+            Dijkstra(matriz, distancias, s, d, s);
+        }
         scanf("%d %d", &n, &m);
     }
     free(saida);
