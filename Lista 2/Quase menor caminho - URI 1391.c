14fda7d9239fdba71023fc133af62b7f456279cf
dayvid-lopes <dayvidlopes@softeam.com.br>
2019-08-29 11:24:24 -0300

    Criação da matriz de adjacência

--- Lista 2/Quase menor caminho - URI 1391.c
+++ Lista 2/Quase menor caminho - URI 1391.c
@@ -1,5 +1,23 @@
 #include "cabeçalhos.h"
+int n;
+FILE *saida;
 
 int main(){
+    saida = fopen("resultados/resultado 1391", "w");
+    int m;
+    scanf("%d %d", &n, &m);
+    while(n != 0 && m != 0){
+        int s, d, u, v, p, i, j;
+        int matriz[n][n], distancias[n];
+        scanf("%d %d", &s, &d);
+        for(i = 0; i < m; i++){
+            scanf("%d %d %d", &u, &v, &p);
+            matriz[u][v] = p;
+            distancias[n] = -1;
+        }
+        
+        scanf("%d %d", &n, &m);
+    }
+    free(saida);
     return 0;
 }
