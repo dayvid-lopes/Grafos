2e4b8ceee13a77986805e8833d6e28187247b540
dayvid-lopes <dayvidlopes@softeam.com.br>
2019-09-03 01:40:59 -0300

    Versão funcional do algoritmo de emparelhamento

--- Lista 2/Minha camiseta me serve - URI 1362.c
+++ Lista 2/Minha camiseta me serve - URI 1362.c
@@ -1,5 +1,6 @@
 #include"cabeçalhos.h"
 FILE *saida;
+int n, m;
 
 typedef struct Voluntario{
     int numero;
@@ -7,12 +8,73 @@ typedef struct Voluntario{
     int tam2;
 }Voluntario;
 
+int *pa;
+int *visited;
+
+void newMatching(int *match, int t) { 
+    int x;
+    do{
+        x = pa[t];
+        match[t] = x;
+        match[x] = t;
+        t = pa[x]; 
+    }while (t != x);
+}
+
+int acharCaminhoDeAumento(int matriz[m][n], int v, int *match){ 
+    int i;
+    for (i = m; i < m + n; i++){
+            //     if(v == 2)
+            // printf("matriz[%d][%d] = %d\n", v, i - m, matriz[v][i - m]);
+        if(matriz[v][i - m] == 1){
+            if (visited[i] == 0){
+                visited[i] = 1;
+
+            //     if(v == 2)
+            // printf("i = %d\t v = %d\n", i, v);
+            if (match[i] == -1 || acharCaminhoDeAumento(matriz, match[i], match) == 1){
+                match[v] = i;
+                match[i] = v;
+                return 1;
+            }
+            }
+        }
+	}
+	return 0;
+}
+
+int UGRAPHbipMatch(int matriz[m][n], int *match){
+    int i, j;
+    visited = MALLOC(int, (m + n));
+    for (i = 0; i < m + n; i++){
+        match[i] = -1;
+        visited[i] = 0;
+    }
+    int size = 0;
+    for(i = 0; i < m; i++){
+        if(match[i] == -1){
+            // printf("match[%d] = %d\n", i, match[i]);
+            size += acharCaminhoDeAumento(matriz, i, match);
+            // printf("match[%d] = %d\n", i, match[i]);
+            for(j = 0; j < m + n; j++){
+                visited[j] = 0;
+            }
+        }
+    }
+    free(visited);
+    // printf("\n");
+    // while (augmentMatch(matriz, color, match)){
+    //     size++;
+    // }
+    return size;
+}
+
 int main(){
     saida = fopen("Lista 2/resultados/resultado 1362", "w");
     int c;
     scanf("%d", &c);
     while (c > 0){
-        int m, n, i, j;
+        int i, j;
         char tam1[4], tam2[4];
         scanf("%d %d", &n, &m);
         int matriz[m][n];
@@ -86,19 +148,23 @@ int main(){
             // tam = i;
             for(j = 0; j < n / 6; j++){
                 matriz[i][tam * n/6 + j] = 1;
-                imprima2("j = %d\ttam * 6 + j = %d\n", j, tam * (n/6) + j);
+                // imprima2("j = %d\ttam * 6 + j = %d\n", j, tam * (n/6) + j);
             }
             
             // imprima2("tam1: %s\ttam2: %s\n", tam1, tam2);
-        imprima("\n");
+        // imprima("\n");
         }
-        for(i = 0; i < m; i++){
-            for(j = 0; j < n; j++){
-                imprima1("%d\t", matriz[i][j]);
-            }
-        imprima("\n");
+        // imprima1("matriz[2][")
+        int color[n + m], match[n + m];
+        // UGRAPHbipMatch(matriz, match);
+        for(i = 0; i < m + n; i++){
+            // for(j = 0; j < n; j++){
+                // imprima1("%d\t", match[i]);
+            // }
+        // imprima("\n");
         }
-        imprima("\n");
+        imprima1("%s\n", UGRAPHbipMatch(matriz, match) == m?"YES":"NO");
+        // imprima("\n");
         c--;
     }
     
